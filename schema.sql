9854f62a2334df4ce94c1dc2dcfd23d0fd2679c5
Taha Hameed <tahahameed17@gmail.com>
2026-04-19 13:01:26 +0200

    Add files via upload

---
+++ schema.sql
@@ -0,0 +1,13 @@
+CREATE TABLE liver_patient(
+    Age int,
+    Gender varchar(50),
+    Total_Bilirubin float,
+    Direct_Bilirubin float,
+    Alkaline_Phosphotase int,
+    Alamine_Aminotransferase int,
+    Aspartate_Aminotransferase int,
+    Total_Protiens float,
+    Albumin float,
+    Albumin_and_Globulin_Ratio float,
+    Dataset int
+);
